5d48a39c6b5a7f41e7914ad93b84cb0225b36d5a
Michael Christenson II <michael@deepwoodsbrigade.com>
2013-04-26 18:46:33 -0400

    added an install script

---
+++ scripts/install.sh
@@ -0,0 +1,45 @@
+echo "Cloning .dotfiles ..."
+hash git >/dev/null && /usr/bin/env git clone https://github.com/m3talsmith/dotfiles.git ~/.dotfiles || {
+  echo "git not installed"
+  exit
+}
+
+echo "Backing up current .dotfiles ..."
+mkdir ~/.dotfiles/backups
+if [ -f ~/.pryrc ] || [ -h ~/.pryrc ]
+then
+  mv ~/.pryrc ~/.dotfiles/backups/
+fi
+if [ -f ~/.rvmrc ] || [ -h ~/.rvmrc ]
+then
+  mv ~/.rvmrc ~/.dotfiles/backups/
+fi
+if [ -f ~/.screenrc ] || [ -h ~/.screenrc ]
+then
+  mv ~/.screenrc ~/.dotfiles/backups/
+fi
+if [ -f ~/.vimrc ] || [ -h ~/.vimrc ]
+then
+  mv ~/.vimrc ~/.dotfiles/backups/
+fi
+if [ -f ~/.vim ] || [ -h ~/.vim ]
+then
+  mv ~/.vim ~/.dotfiles/backups/
+fi
+if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]
+then
+  mv ~/.zshrc ~/.dotfiles/backups/
+fi
+
+echo "Linking .dotfiles ..."
+ln -s ~/.dotfiles/.pryrc ~/.pryrc
+ln -s ~/.dotfiles/.rvmrc ~/.rvmrc
+ln -s ~/.dotfiles/.screenrc ~/.screenrc
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.vim ~/.vim
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+
+echo "Resourcing ..."
+source ~/.zshrc
+
+echo "Good to go!"
